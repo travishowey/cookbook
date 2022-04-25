0cb3acda84dde6b60795f1a18a151b8dd11648b7
travishowey <topfueltrav969@gmail.com>
2022-04-25 15:40:23 -0600

    added images

--- mysite/howeydoin/urls.py
+++ mysite/howeydoin/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<int:item_id>', views.detail, name="detail"),
     path('recipes/', views.allrecipes, name="allrecipes"),
     path('aboutus/', views.aboutus, name="aboutus"),
-    path('download/<int:id>/', views.download, name="download"),
+    path('download/<int:item_id>', views.download, name="download"),
     path('questions', views.questions, name="questions"),
     path('add', views.create_item, name='create_item'),
 ]
